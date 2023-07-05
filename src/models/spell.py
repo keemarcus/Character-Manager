0e7ea254c1a438a27a9388a0e34638e76e776660
Marcus Kee <62085649+keemarcus@users.noreply.github.com>
2023-07-04 22:36:54 -0400

    reworked spell class to work with new database setup

--- src/models/spell.py
+++ src/models/spell.py
@@ -4,56 +4,43 @@ from json import JSONEncoder
 
 class Spell:
     # constructor
-    def __init__(self, index, name, desc, spell_range, components, duration, concentration, casting_time, level, school,
-                 classes, subclasses, dc, higher_level, ritual, damage, attack_type):
+    def __init__(self, index, user_id, name, level, classes, subclasses, school, casting_time, range, duration,
+                 components, material, concentration, desc, ritual, dc, higher_level, damage, area_of_effect,
+                 heal_at_slot_level, attack_type):
         self._index = index
+        self._user_id = user_id
         self._name = name
-        self._desc = desc
-        self._spell_range = spell_range
-        self._components = components
-        self._duration = duration
-        self._concentration = concentration
-        self._casting_time = casting_time
         self._level = level
-        self._school = school
         self._classes = classes
         self._subclasses = subclasses
+        self._school = school
+        self._casting_time = casting_time
+        self._range = range
+        self._duration = duration
+        self._components = components
+        self._material = material
+        self._concentration = concentration
+        self._desc = desc
+        self._ritual = ritual
         self._dc = dc
         self._higher_level = higher_level
-        self._ritual = ritual
         self._damage = damage
+        self._area_of_effect = area_of_effect
+        self._heal_at_slot_level = heal_at_slot_level
         self._attack_type = attack_type
 
     # getters
     def get_index(self):
         return self._index
 
+    def get_user_id(self):
+        return self._user_id
+
     def get_name(self):
         return self._name
 
-    def get_desc(self):
-        return self._desc
-
-    def get_range(self):
-        return self._spell_range
-
-    def get_components(self):
-        return self._components
-
-    def get_duration(self):
-        return self._duration
-
-    def get_concentration(self):
-        return self._concentration
-
-    def get_casting_time(self):
-        return self._casting_time
-
     def get_level(self):
         return self._level
-
-    def get_school(self):
-        return self._school
 
     def get_classes(self):
         return self._classes
@@ -61,17 +48,47 @@ class Spell:
     def get_subclasses(self):
         return self._subclasses
 
+    def get_school(self):
+        return self._school
+
+    def get_casting_time(self):
+        return self._casting_time
+
+    def get_range(self):
+        return self._range
+
+    def get_duration(self):
+        return self._duration
+
+    def get_components(self):
+        return self._components
+
+    def get_material(self):
+        return self._material
+
+    def get_concentration(self):
+        return self._concentration
+
+    def get_desc(self):
+        return self._desc
+
+    def get_ritual(self):
+        return self._ritual
+
     def get_dc(self):
         return self._dc
 
     def get_higher_level(self):
         return self._higher_level
 
-    def get_ritual(self):
-        return self._ritual
-
     def get_damage(self):
         return self._damage
+
+    def get_area_of_effect(self):
+        return self._area_of_effect
+
+    def get_heal_at_slot_level(self):
+        return self._heal_at_slot_level
 
     def get_attack_type(self):
         return self._attack_type
@@ -80,32 +97,14 @@ class Spell:
     def set_index(self, index):
         self._index = index
 
+    def set_user_id(self, user_id):
+        self._user_id = user_id
+
     def set_name(self, name):
         self._name = name
 
-    def set_desc(self, desc):
-        self._desc = desc
-
-    def set_range(self):
-        self._spell_range = range
-
-    def set_components(self, components):
-        self._components = components
-
-    def set_duration(self, duration):
-        self._duration = duration
-
-    def set_concentration(self, concentration):
-        self._concentration = concentration
-
-    def set_casting_time(self,casting_time):
-        self._casting_time = casting_time
-
     def set_level(self, level):
         self._level = level
-
-    def set_school(self, school):
-        self._school = school
 
     def set_classes(self, classes):
         self._classes = classes
@@ -113,17 +112,47 @@ class Spell:
     def set_subclasses(self, subclasses):
         self._subclasses = subclasses
 
+    def set_school(self, school):
+        self._school = school
+
+    def set_casting_time(self, casting_time):
+        self._casting_time = casting_time
+
+    def set_range(self, range):
+        self._range = range
+
+    def set_duration(self, duration):
+        self._duration = duration
+
+    def set_components(self, components):
+        self._components = components
+
+    def set_material(self, material):
+        self._material = material
+
+    def set_concentration(self, concentration):
+        self._concentration = concentration
+
+    def set_desc(self, desc):
+        self._desc = desc
+
+    def set_ritual(self, ritual):
+        self._ritual = ritual
+
     def set_dc(self, dc):
         self._dc = dc
 
     def set_higher_level(self, higher_level):
         self._higher_level = higher_level
 
-    def set_ritual(self, ritual):
-        self._ritual = ritual
-
     def set_damage(self, damage):
         self._damage = damage
+
+    def set_area_of_effect(self, area_of_effect):
+        self._area_of_effect = area_of_effect
+
+    def set_heal_at_slot_level(self, heal_at_slot_level):
+        self._heal_at_slot_level = heal_at_slot_level
 
     def set_attack_type(self, attack_type):
         self._attack_type = attack_type
--- utils/populate_spells.py
+++ utils/populate_spells.py
@@ -1,5 +1,6 @@
 from utils import dbconfig
 import requests
+from src.models.spell import Spell
 import json
 
 spell_count = 0
@@ -7,37 +8,76 @@ spell_count = 0
 spells = requests.get("https://www.dnd5eapi.co/api/spells").json()
 for spell in spells["results"]:
     spell_count += 1
-    spell_data = {'casting_time': None,
-                  'attack_type': None,
-                  'range': None,
-                  'duration': None,
-                  'components': None,
-                  'damage': None,
-                  'dc': None,
-                  'higher_level': None,
-                  'ritual': None,
-                  'classes': None,
-                  'name': None,
-                  'school': None,
-                  'area_of_effect': None,
-                  'heal_at_slot_level': None,
-                  'desc': None,
-                  'concentration': None,
-                  'level': None,
-                  'material': None,
-                  'index': None,
-                  'subclasses': None}
+    # spell_data = {'casting_time': None,
+    #               'attack_type': None,
+    #               'range': None,
+    #               'duration': None,
+    #               'components': None,
+    #               'damage': None,
+    #               'dc': None,
+    #               'higher_level': None,
+    #               'ritual': None,
+    #               'classes': None,
+    #               'name': None,
+    #               'school': None,
+    #               'area_of_effect': None,
+    #               'heal_at_slot_level': None,
+    #               'desc': None,
+    #               'concentration': None,
+    #               'level': None,
+    #               'material': None,
+    #               'index': None,
+    #               'subclasses': None}
+    spell_datax = Spell(None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None,
+                        None, None, None, None, None)
     spell_info = requests.get("https://www.dnd5eapi.co/api/spells/" + spell["index"]).json()
     for field in spell_info:
-        if field in ['classes', 'subclasses']:
+        if field == 'index':
+            spell_datax.set_index(str(spell_info[field]))
+        elif field == 'name':
+            spell_datax.set_name(str(spell_info[field]))
+        elif field == 'level':
+            spell_datax.set_level(str(spell_info[field]))
+        elif field == 'classes':
             data = ""
             for entry in spell_info[field]:
                 data += entry["index"] + " - "
-            spell_data[field] = data[:-3]
+            spell_datax.set_classes(data[:-3])
+        elif field == 'subclasses':
+            data = ""
+            for entry in spell_info[field]:
+                data += entry["index"] + " - "
+            spell_datax.set_subclasses(data[:-3])
         elif field == 'school':
-            spell_data[field] = spell_info[field]['index']
+            spell_datax.set_school(spell_info[field]['index'])
+        elif field == 'casting_time':
+            spell_datax.set_casting_time(str(spell_info[field]))
+        elif field == 'range':
+            spell_datax.set_range(str(spell_info[field]))
+        elif field == 'duration':
+            spell_datax.set_duration(str(spell_info[field]))
+        elif field == 'components':
+            spell_datax.set_components(str(spell_info[field]))
+        elif field == 'materials':
+            spell_datax.set_materials(str(spell_info[field]))
+        elif field == 'concentration':
+            spell_datax.set_concentration(str(spell_info[field]))
+        elif field == 'desc':
+            spell_datax.set_desc(str(spell_info[field]))
+        elif field == 'ritual':
+            spell_datax.set_ritual(str(spell_info[field]))
+        elif field == 'dc':
+            spell_datax.set_dc(str(spell_info[field]))
+        elif field == 'higher_level':
+            spell_datax.set_higher_level(str(spell_info[field]))
+        elif field == 'damage':
+            spell_datax.set_damage(str(spell_info[field]))
+        elif field == 'area_of_effect':
+            spell_datax.set_area_of_effect(str(spell_info[field]))
+        elif field == 'heal_at_slot_level':
+            spell_datax.set_heal_at_slot_level(str(spell_info[field]))
         else:
-            spell_data[field] = str(spell_info[field])
+            spell_datax.set_attack_type(str(spell_info[field]))
 
     # create result variable
     result = None
@@ -57,7 +97,7 @@ for spell in spells["results"]:
 
     #print(result)
     if result is None:
-        print("Adding Spell: " + spell_data['name'])
+        print("Adding Spell: " + spell_datax.get_name())
         try:
             # set up a new database connection and cursor
             connection = dbconfig.get_connection()
@@ -65,17 +105,17 @@ for spell in spells["results"]:
             # create query string using parameterization to protect against SQL injection
             query = "INSERT INTO spells VALUES (?, 0, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
             # execute our query and commit the changes to the database
-            cursor.execute(query, spell_data['index'], spell_data['name'], spell_data['level'], spell_data['classes'],
-                           spell_data['subclasses'], spell_data['school'], spell_data['casting_time'],
-                           spell_data['range'], spell_data['duration'], spell_data['components'],
-                           spell_data['material'], spell_data['concentration'], spell_data['desc'],
-                           spell_data['ritual'], spell_data['dc'], spell_data['higher_level'], spell_data['damage'],
-                           spell_data['area_of_effect'], spell_data['heal_at_slot_level'], spell_data['attack_type'])
+            cursor.execute(query, spell_datax.get_index(), spell_datax.get_name(), spell_datax.get_level(), spell_datax.get_classes(),
+                           spell_datax.get_subclasses(), spell_datax.get_school(), spell_datax.get_casting_time(),
+                           spell_datax.get_range(), spell_datax.get_duration(), spell_datax.get_components(),
+                           spell_datax.get_material(), spell_datax.get_concentration(), spell_datax.get_desc(),
+                           spell_datax.get_ritual(), spell_datax.get_dc(), spell_datax.get_higher_level(), spell_datax.get_damage(),
+                           spell_datax.get_area_of_effect(), spell_datax.get_heal_at_slot_level(), spell_datax.get_attack_type())
             cursor.commit()
         finally:
             # close our database connection
             connection.close()
     else:
-        print("Spell: " + spell_data['name'] + " - Is Already In Database")
+        print("Spell: " + spell_datax.get_name() + " - Is Already In Database")
 
 print("Number of Spells: " + spell_count.__str__())
