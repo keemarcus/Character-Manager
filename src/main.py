8926c349e62c486de37d847bb0d4c6f167caae0b
Marcus Kee <62085649+keemarcus@users.noreply.github.com>
2023-10-17 23:28:29 -0400

    adding session type config

--- src/main.py
+++ src/main.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     gunicorn_logger = logging.getLogger('gunicorn.error')
     app.logger.handlers = gunicorn_logger.handlers
     app.logger.setLevel(gunicorn_logger.level)
+    app.config['SESSION_TYPE'] = 'memcached'
     app.secret_key = 'super_secret'
     #session['user_id'] = 1
     app.run()
